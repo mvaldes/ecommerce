f6bcee1d78dd0d2f63b2e9dc3bef09f799529172
Mauro Valdes <mvrodrigo40@hotmail.com>
2024-02-20 15:26:17 -0500

    init of env variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
+import { API_URL } from "../env"
 
 function App() {
-
   return (
     <>
       <div>
-        <h1>Hola mundo</h1>
+        <h1>Hola mundo {API_URL}</h1>
       </div> 
     </>
   )
